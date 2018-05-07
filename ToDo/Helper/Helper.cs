2faa9d811f82d312f4d2a80e671d438d89203018
Ashish Gangal <ashish.gangal1990@gmail.com>
2018-05-07 16:15:01 -0700

    initial layout

---
+++ ToDo/Helper/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Helper
+{
+    public class Helper
+    {
+        #region File helper
+
+        public void WriteFile(string filename, string data)
+        {
+
+        }
+        
+        public string ReadFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ ToDo/Model/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Model
+{
+    public class Item
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ItemState State { get; set; }
+    }
+
+    public enum ItemState
+    {
+        NotStarted,
+        InProgress,
+        Paused,
+        Abandoned,
+        Completed
+    }
+
+
+}
