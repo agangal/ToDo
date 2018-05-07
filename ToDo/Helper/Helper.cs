7a322c58b64d15749d547832bc6227099edd25a9
Ashish Gangal <ashish.gangal1990@gmail.com>
2018-05-07 16:20:47 -0700

    add method layout

--- ToDo/Helper/Helper.cs
+++ ToDo/Helper/Helper.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToDo.Helper
 {
+    using Model;
     public class Helper
     {
         #region File helper
@@ -18,6 +19,54 @@ namespace ToDo.Helper
         public string ReadFile(string filename)
         {
             throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region methods offering up data
+
+        public void CurrentItems()
+        {
+
+        }
+
+        public void CurrentItems(DateTime startDate)
+        {
+
+        }
+
+        public void CurrentItems(DateTime fromDate, DateTime toDate)
+        {
+
+        }
+
+        public void PausedItems()
+        {
+
+        }
+
+        public void AllItemsByDate()
+        {
+
+        }
+
+        #endregion
+
+        #region methods adding/updating items
+
+        public void AddNewItem(Item item)
+        {
+
+        }
+
+        public void  UpdateItem(Item item)
+        {
+
+        }
+
+        public void UpdateItemState(Item item, ItemState newState)
+        {
+
         }
 
         #endregion
